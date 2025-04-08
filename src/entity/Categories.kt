3be0fb19d24ac0cda4e57edd879d2a87e9a860d1
Mostafa Mohammed <tofymofy.55@gmail.com>
2025-04-08 23:23:58 +0300

    feat:add category enum class

---
+++ src/entity/Categories.kt
@@ -0,0 +1,14 @@
+package entity
+
+enum class Categories {
+    Food,
+    Rent,
+    Salary,
+    Taxes,
+    HealthInsurance,
+    DentalInsurance,
+    Electricity,
+    Water,
+    Internet,
+    PhoneBill,
+}
