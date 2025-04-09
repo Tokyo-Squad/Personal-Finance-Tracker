8a63dd73c5d87a2dc805750bbd14163ab0d5c65e
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-04-09 13:40:02 +0200

    add transaction data class with enum types

--- src/entity/Transaction.kt
+++ src/entity/Transaction.kt
@@ -1,7 +1,26 @@
 package entity
 
+import java.time.LocalDateTime
+
+/**
+ * @param id Transaction's unique ID.
+ * @param date Date of the transaction.
+ * @param money Details of the transaction amount.
+ * @param category Details of the transaction category.
+ * @param description Brief details of the transaction.
+ * @param type Indicates if it's INCOME or EXPENSE.
+ */
 data class Transaction(
-    val id :Int,
-    val amount :Float,
-    val category: String,
-)
+    val id: Int,
+    val date: LocalDateTime,
+    val money: Money,
+    val category: Categories,
+    val description: String?,
+    val type: Type
+) {
+
+    enum class Type {
+        INCOME,
+        EXPENSE
+    }
+}
