2f44cce3ded395659646283fe792651b9cb1785b
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2025-04-10 20:43:20 +0200

    add test execution logic to main function

--- src/Main.kt
+++ src/Main.kt
@@ -1,1 +1,16 @@
-fun main() {}
+import test.feature.InMemoryTransactionTestCases
+import test.feature.monthlyReportTestCases
+
+fun main(args: Array<String>) {
+    if (args[0]=="test1"){
+        val test = InMemoryTransactionTestCases()
+        test.runAllTests()
+    }
+    else if (args[1] == "test2"){
+        monthlyReportTestCases()
+    }
+    else{
+        //Program logic
+    }
+
+}
