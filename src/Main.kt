0543789f9e21b84d88785524712517cbccd3192c
Hasan Ali <ihasanalihasan@gmail.com>
2025-04-10 23:31:11 +0300

    add console logic and printer to main

--- src/Main.kt
+++ src/Main.kt
@@ -2,52 +2,102 @@ import entity.CategoryEntity
 import entity.MoneyEntity
 import entity.TransactionEntity
 import transaction.FileTransaction
+import utils.checkCategoryType
 import java.time.LocalDateTime
 import java.util.*
+
+
 
 fun main() {
     val scanner = Scanner(System.`in`)
     val transactionManager = TransactionManager(FileTransaction())
+    val console = PrinterConsole()
 
-    println("Welcome to the Transaction Manager")
+    console.printWelcomeMessage()
 
     while (true) {
-        println("\nMenu:")
-        println("1. Add a new transaction")
-        println("2. Show monthly summary")
-        println("3. Exit")
-        print("Please choose an option (1/2/3): ")
-
+        console.printMenuList()
         when (scanner.nextInt()) {
             1 -> {
 
                 scanner.nextLine()
-                print("Enter transaction ID (Int): ")
+                console.printTitleMessage("Enter transaction ID (Int): ")
                 val id = scanner.nextInt()
                 scanner.nextLine()
-
-                print("Enter amount (Double): ")
+                console.printTitleMessage("Enter amount (Double): ")
                 val amount = scanner.nextDouble()
                 scanner.nextLine()
 
-                print("Enter currency (e.g., USD): ")
+                console.printTitleMessage("Enter currency (e.g., USD): ")
                 val currency = scanner.nextLine()
 
-                print("Enter category name: ")
-                val categoryName = scanner.nextLine()
+                console.printTitleMessage("Enter category name: ")
+                val category = scanner.nextLine()
+                val categoryName = checkCategoryType(category)
 
-                print("Enter description: ")
+
+                console.printTitleMessage("Enter description: ")
                 val description = scanner.nextLine()
 
-                print("Enter type (INCOME or EXPENSE): ")
+                console.printTitleMessage("Enter type (INCOME or EXPENSE): ")
                 val typeInput = scanner.nextLine().uppercase()
                 val type = try {
                     TransactionEntity.Type.valueOf(typeInput)
                 } catch (e: IllegalArgumentException) {
-                    println("Invalid type. Please enter INCOME or EXPENSE.")
+                    console.printErrorMessage("Invalid type. Please enter INCOME or EXPENSE.")
                     continue
                 }
 
+                val transaction = TransactionEntity(
+                    id = id,
+                    date = LocalDateTime.now(),
+                    money = MoneyEntity(amount, MoneyEntity.Currency.valueOf(currency.uppercase())),
+                    category = categoryName,
+                    description = description,
+                    type = type
+                )
+
+                val added = transactionManager.add(transaction)
+                if (added){
+                    console.printSuccessOperation( "✅ Transaction added.")
+                } else console.printErrorMessage("❌ Transaction ID already exists!")
+
+            }
+            2 -> {
+                console.printTitleMessage("Enter the month (1-12): ")
+                val month = scanner.nextInt()
+                val monthlyReportSummary = transactionManager.getMonthlyReport(month = month)
+                println(monthlyReportSummary)
+                console.printSuccessOperation("✅ monthly report got")
+
+            }
+            3-> {
+
+                console.printTitleMessage("Enter transaction ID (Int): ")
+                val id = scanner.nextInt()
+                scanner.nextLine()
+
+                console.printTitleMessage("Enter amount (Double): ")
+                val amount = scanner.nextDouble()
+                scanner.nextLine()
+
+                console.printTitleMessage("Enter currency (e.g., USD): ")
+                val currency = scanner.nextLine()
+
+                console.printTitleMessage("Enter category name: ")
+                val categoryName = scanner.nextLine()
+
+                console.printTitleMessage("Enter description: ")
+                val description = scanner.nextLine()
+
+                console.printTitleMessage("Enter type (INCOME or EXPENSE): ")
+                val typeInput = scanner.nextLine().uppercase()
+                val type = try {
+                    TransactionEntity.Type.valueOf(typeInput)
+                } catch (e: IllegalArgumentException) {
+                    console.printErrorMessage("Invalid type. Please enter INCOME or EXPENSE.")
+                    continue
+                }
                 val transaction = TransactionEntity(
                     id = id,
                     date = LocalDateTime.now(),
@@ -56,27 +106,29 @@ fun main() {
                     description = description,
                     type = type
                 )
-
-                val added = transactionManager.add(transaction)
-                println(if (added) "✅ Transaction added." else "❌ Transaction ID already exists!")
+                    if(transactionManager.update(transaction))
+                        console.printSuccessOperation(" ✅Transaction Updated ... ")
+                    else console.printErrorMessage("❌ There is Some Wrong Not Value Updated ...  ")
+            }
+            4 -> {
+                scanner.nextLine()
+                val idValue = scanner.nextInt()
+                scanner.nextLine()
+                if(transactionManager.delete(idValue))
+                    console.printSuccessOperation(" ✅Transaction Deleted ... ")
+                else console.printErrorMessage("❌ There is Some Wrong Not Value Deleted ... ")
 
             }
-            2 -> {
-
-                println("Enter the month (1-12): ")
-                val month = scanner.nextInt()
-                println("Enter the year (e.g., 2025): ")
-                val year = scanner.nextInt()
-
-
+            5-> {
+                console.printAllTransaction(transactionManager.getAll())
+                console.printSuccessOperation("✅ all transaction got")
             }
-            3 -> {
-
-                println("Goodbye! 👋")
+            6-> {
+                console.printSuccessOperation("Goodbye! 👋")
                 break
             }
             else -> {
-                println("Invalid option. Please try again.")
+                console.printErrorMessage("Invalid option. Please try again.")
             }
         }
     }
