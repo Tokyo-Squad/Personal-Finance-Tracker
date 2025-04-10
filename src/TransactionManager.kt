b44b796f200af8de9637702b940b422129ba465e
Hasan Ali <ihasanalihasan@gmail.com>
2025-04-10 23:30:20 +0300

    update transaction manager

--- src/TransactionManager.kt
+++ src/TransactionManager.kt
@@ -1,28 +1,39 @@
+import entity.MonthReportEntity
 import entity.TransactionEntity
+import monthreport.MonthlyReportCalculator
+import monthreport.MonthlyReportCalculatorImp
+import monthreport.filter.TransactionFilter
+import monthreport.filter.TransactionFilterImp
 import transaction.Transaction
 
 
-class TransactionManager (private val transaction: Transaction){
+class TransactionManager (
+    private val transaction: Transaction,
+    private val transactionFilter: TransactionFilter= TransactionFilterImp(),
+    private val monthlyReportCalculator: MonthlyReportCalculator = MonthlyReportCalculatorImp(transactions = transaction.getAll(), filter = transactionFilter)
 
-
-
+    ){
 
     fun add(transactionEntity: TransactionEntity): Boolean {
-
         transaction.add(transactionEntity)
         return true
     }
 
+    fun getMonthlyReport(month: Int): MonthReportEntity {
+        return monthlyReportCalculator.calculateMonthlyReport(month)
+    }
+
+
     fun getAll(): List<TransactionEntity> = transaction.getAll()
+
     fun update(transactionEntity:TransactionEntity ): Boolean {
         transaction.update(transactionEntity)
         return true
     }
 
-
     fun delete(id: Int): Boolean {
         return transaction.delete(id)
     }
 
-    }
+}
 
